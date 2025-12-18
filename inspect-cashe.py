aab827543bd231906383deaf74a515cf67b6f66c
renelarsson <116158099+renelarsson@users.noreply.github.com>
2025-12-18 07:37:46 +0000

    remove large files and directories from Git tracking

--- inspect-cashe.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Load the cache file
-cache_file = "/workspaces/ppe-compliance-detection/data/labels/train.cache"
-data = np.load(cache_file, allow_pickle=True)
-
-# Inspect the contents
-print(data)
